f60ad8ce060d8799efb71c30fda37bc5e1ddb510
kynnedye <90293916+kynnedye@users.noreply.github.com>
2022-01-11 21:24:34 -0500

    contact form

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const navToggle = document.querySelector(".nav-toggle")
 const navLink = document.querySelectorAll(".nav__link")
 const contactBtn = document.getElementById("contact-btn")
 const message = document.getElementById("message")
+const social = document.getElementById("follow")
+const socialBtn = document.getElementById("footer-social")
 
 navToggle.addEventListener("click", function(){
     document.body.classList.toggle('nav-open');
@@ -17,4 +19,7 @@ contactBtn.addEventListener("click",function(){
     document.getElementById("form").style.display = "block"
     contactBtn.style.display = "none"
     message.textContent = "Get in touch"
+    socialBtn.style.display = "flex"
+    social.style.display = "none"
+    location.href="index.html#form"
 })
