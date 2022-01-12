c95cc02ab1b15b2a622b2d7179abca907a1ba481
kynnedye <90293916+kynnedye@users.noreply.github.com>
2022-01-11 20:47:26 -0500

    add contact

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const navToggle = document.querySelector(".nav-toggle")
 const navLink = document.querySelectorAll(".nav__link")
+const contactBtn = document.getElementById("contact-btn")
+const message = document.getElementById("message")
 
 navToggle.addEventListener("click", function(){
     document.body.classList.toggle('nav-open');
@@ -9,4 +11,10 @@ navLink.forEach(link => {
     link.addEventListener("click", function(){
         document.body.classList.remove('nav-open');
     })
+})
+
+contactBtn.addEventListener("click",function(){
+    document.getElementById("form").style.display = "block"
+    contactBtn.style.display = "none"
+    message.textContent = "Get in touch"
 })
